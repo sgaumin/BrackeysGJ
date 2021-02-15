eb20800835ef8f853dc9f9cd49d7dd8075515822
Sebastien Gm <sgaumin@gmail.com>
2021-02-15 21:36:34 +0900

    [Feature] Rat base movements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private LayerMask movementLayer;
+	[SerializeField] private string inputName;
+
+	public Vector3 Destination { get; private set; }
+
+	private RaycastHit hit;
+	private Ray ray;
+
+	private void Update()
+	{
+		if (string.IsNullOrEmpty(inputName))
+		{
+			RetreiveDestination();
+		}
+		else if (Input.GetButton(inputName))
+		{
+			RetreiveDestination();
+		}
+	}
+
+	private void RetreiveDestination()
+	{
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit, Mathf.Infinity, movementLayer, QueryTriggerInteraction.Ignore))
+		{
+			Destination = hit.point;
+		}
+	}
+}
---
+++ Assets/Scripts/Rat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Rat : MonoBehaviour
+{
+	[Header("References")]
+	[SerializeField] private NavMeshAgent agent;
+
+	private PlayerController player;
+
+	protected void Start()
+	{
+		player = FindObjectOfType<PlayerController>();
+	}
+
+	private void Update()
+	{
+		agent.SetDestination(player.Destination);
+	}
+}
