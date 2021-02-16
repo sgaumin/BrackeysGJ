84ec50b71c0a16c936edc5a09cfdf0602cc07c77
Sebastien Gm <sgaumin@gmail.com>
2021-02-16 21:15:04 +0900

    [Feature] Spawn a mass of rat

---
+++ Assets/Scripts/LevelSetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSetter : MonoBehaviour
+{
+  [Header("Level Parameters")]
+  [SerializeField] private int ratCountAtStart = 6;
+
+  [Header("References")]
+  [SerializeField] private Rat ratPrefab;
+  [SerializeField] private Transform ratHolder;
+
+  protected void Start()
+  {
+    for (int i = 0; i < ratCountAtStart; i++)
+    {
+      Instantiate(ratPrefab, ratHolder);
+    }
+  }
+}
