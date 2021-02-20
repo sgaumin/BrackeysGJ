ea6f920365f14d06a3c63ce7f13e938e26466f5e
Kikkawa <sylve.renault@yahoo.fr>
2021-02-20 15:20:35 +0100

    [Features]Affichage des crédits dans le menu

---
+++ Assets/Scripts/CreditsLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsLoader : MonoBehaviour
+{
+    [SerializeField]
+    GameObject credits;
+    bool creditsState = false;
+    private void Start()
+    {
+        credits.SetActive(false);
+    }
+    // Start is called before the first frame update
+    public void CreditsHide()
+    {
+        if(creditsState == false)
+        {
+            credits.SetActive(true);
+            creditsState = true;
+        }
+        else
+        {
+            credits.SetActive(false);
+            creditsState = false;
+        }
+    }
+}
