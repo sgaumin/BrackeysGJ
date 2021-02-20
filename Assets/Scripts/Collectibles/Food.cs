91cf6199922eb0c4035a42222096e0b7b183914e
Kikkawa <sylve.renault@yahoo.fr>
2021-02-20 11:01:07 +0100

    [Features]Food Collectibles

---
+++ Assets/Scripts/Collectibles/Food.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    [SerializeField]
+    int nbrOfRatToAdd = 10;
+    [SerializeField] private Rat ratPrefab;
+    Transform ratHolder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ratHolder = GameObject.Find("RatHolder").transform;
+        Debug.Log("ratholder" + ratHolder);
+        Debug.Log("ratPrefab" + ratPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // on trigger, add rats to the horde
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 8)
+        {
+            Destroy(this.gameObject);
+            for (int i = 0; i < nbrOfRatToAdd; i++)
+            {
+                Instantiate(ratPrefab, ratHolder);
+            }
+            Debug.Log("hello world");
+        }
+    }
+}
