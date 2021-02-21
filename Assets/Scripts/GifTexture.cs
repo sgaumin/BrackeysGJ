9dd68d69f4d9986cc29971a28036ab6b18663b98
Sebastien Gm <sgaumin@gmail.com>
2021-02-21 17:31:18 +0900

    [Graphics] Texture integration

---
+++ Assets/Scripts/GifTexture.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GifTexture : MonoBehaviour
+{
+	[SerializeField] private float tileX = 1;
+	[SerializeField] private float tileY = 1;
+
+	private Mesh mesh;
+	private Material mat;
+
+	void Start()
+	{
+		mat = GetComponent<Renderer>().material;
+		mesh = GetComponent<MeshFilter>().mesh;
+	}
+
+	void Update()
+	{
+		mat.mainTextureScale = new Vector2((mesh.bounds.size.x * transform.localScale.x) / 100 * tileX, (mesh.bounds.size.y * transform.localScale.y) / 100 * tileY);
+	}
+}
