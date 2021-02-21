fa25a03b43dcb4417487c5f4c55b21cef2416ca9
Kikkawa <sylve.renault@yahoo.fr>
2021-02-21 01:39:57 +0100

    [Features]TimeManager

---
+++ Assets/TimeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    float timeInSeconds;
+    [SerializeField]
+    Text timer;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        launchTimer();
+    }
+    public void launchTimer()
+    {
+        timeInSeconds -= Time.deltaTime % 60;
+        float timeLeft = Mathf.RoundToInt(timeInSeconds);
+
+        if (timeInSeconds <= 0)
+        {
+            // Debug.Log("prout");
+            //Do finish timer
+        }
+        else
+        {
+            timer.text = timeLeft + "";
+            //Debug.Log(timeInSeconds);
+        }
+    }
+}
